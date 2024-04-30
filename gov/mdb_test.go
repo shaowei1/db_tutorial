bd74b2257732979538bf99bdd279c8a44d397de1
shaowei <shaowei_8910@163.com>
2024-04-30 19:41:38 +0800

    Part 8 - B-Tree Leaf Node Format

--- gov/mdb_test.go
+++ gov/mdb_test.go
@@ -9,6 +9,73 @@ import (
 	"testing"
 )
 
+// 在测试函数开始前创建数据库文件，在测试结束后删除数据库文件
+func setupAndTeardownDBFile() string {
+	dbPath := createAndRemoveDBFile()
+	defer os.Remove(dbPath)
+	return dbPath
+}
+
+func TestPrintBTreeStructure(t *testing.T) {
+	setupAndTeardownDBFile()
+	script := []string{
+		"insert 3 user3 person3@example.com",
+		"insert 1 user1 person1@example.com",
+		"insert 2 user2 person2@example.com",
+		".btree",
+		".exit",
+	}
+
+	// 运行脚本并获取输出结果
+	result := runScript(script)
+
+	// 期望的输出结果
+	expected := []string{
+		"db > Executed.",
+		"db > Executed.",
+		"db > Executed.",
+		"db > Tree:",
+		"leaf (size 3)",
+		"  - 0 : 3",
+		"  - 1 : 1",
+		"  - 2 : 2",
+		"db > ",
+	}
+
+	// 检查输出结果是否符合预期
+	if !isEqual(result, expected) {
+		t.Errorf("Expected output: %v, got: %v", expected, result)
+	}
+}
+
+func TestPrintConstants(t *testing.T) {
+	// 准备测试脚本
+	script := []string{
+		".constants",
+		".exit",
+	}
+
+	// 运行脚本并获取输出结果
+	result := runScript(script)
+
+	// 期望的输出结果
+	expected := []string{
+		"db > Constants:",
+		"ROW_SIZE: 293",
+		"COMMON_NODE_HEADER_SIZE: 6",
+		"LEAF_NODE_HEADER_SIZE: 10",
+		"LEAF_NODE_CELL_SIZE: 297",
+		"LEAF_NODE_SPACE_FOR_CELLS: 4086",
+		"LEAF_NODE_MAX_CELLS: 13",
+		"db > ",
+	}
+
+	// 检查输出结果是否符合预期
+	if !isEqual(result, expected) {
+		t.Errorf("Expected output: %v, got: %v", expected, result)
+	}
+}
+
 func TestKeepsDataAfterClosingConnection(t *testing.T) {
 	setupAndTeardownDBFile()
 
@@ -194,12 +261,5 @@ func isEqual(a, b []string) bool {
 func createAndRemoveDBFile() string {
 	dbPath := "test.db"
 	os.Remove(dbPath) // 删除已存在的数据库文件
-	return dbPath
-}
-
-// 在测试函数开始前创建数据库文件，在测试结束后删除数据库文件
-func setupAndTeardownDBFile() string {
-	dbPath := createAndRemoveDBFile()
-	defer os.Remove(dbPath)
 	return dbPath
 }
