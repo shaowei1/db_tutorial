ac4f763fc0404ef186b038eb60b941ed157b906b
shaowei <shaowei_8910@163.com>
2024-05-05 16:47:32 +0800

    Part 13 - Updating Parent Node After a Split

--- mdb.c
+++ mdb.c
@@ -213,6 +213,9 @@ const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CELL_SIZE =
     INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
+/* Keep this small for testing */
+const uint32_t INTERNAL_NODE_MAX_CELLS = 3;
+
 uint32_t *internal_node_num_keys(void *node) {
   return node + INTERNAL_NODE_NUM_KEYS_OFFSET;
 }
@@ -238,7 +241,7 @@ uint32_t *internal_node_child(void *node, uint32_t child_num) {
 }
 
 uint32_t *internal_node_key(void *node, uint32_t key_num) {
-  return internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE;
+  return (void *)internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE;
 }
 
 NodeType get_node_type(void *node) {
@@ -339,6 +342,35 @@ void *get_page(Pager *pager, uint32_t page_num) {
   return pager->pages[page_num];
 }
 
+uint32_t *node_parent(void *node) { return node + PARENT_POINTER_OFFSET; }
+
+uint32_t internal_node_find_child(void *node, uint32_t key) {
+  /*
+  Return the index of the child which should contain
+  the given key.
+  */
+  uint32_t num_keys = *internal_node_num_keys(node);
+  /* Binary search */
+  uint32_t min_index = 0;
+  uint32_t max_index = num_keys; /* there is one more child than key */
+
+  while (min_index != max_index) {
+    uint32_t index = (min_index + max_index) / 2;
+    uint32_t key_to_right = *internal_node_key(node, index);
+    if (key_to_right >= key) {
+      max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+  return min_index;
+}
+
+void update_internal_node_key(void *node, uint32_t old_key, uint32_t new_key) {
+  uint32_t old_child_index = internal_node_find_child(node, old_key);
+  *internal_node_key(node, old_child_index) = new_key;
+}
+
 void create_new_root(Table *table, uint32_t right_child_page_num) {
   /*
   Handle splitting the root.
@@ -363,6 +395,8 @@ void create_new_root(Table *table, uint32_t right_child_page_num) {
   uint32_t left_child_max_key = get_node_max_key(left_child);
   *internal_node_key(root, 0) = left_child_max_key;
   *internal_node_right_child(root) = right_child_page_num;
+  *node_parent(left_child) = table->root_page_num;
+  *node_parent(right_child) = table->root_page_num;
 }
 
 void serialize_row(Row *source, void *destination) {
@@ -377,6 +411,45 @@ void deserialize_row(void *source, Row *destination) {
   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+void internal_node_insert(Table *table, uint32_t parent_page_num,
+                          uint32_t child_page_num) {
+  /*
+  Add a new child/key pair to parent that corresponds to child
+  */
+
+  void *parent = get_page(table->pager, parent_page_num);
+  void *child = get_page(table->pager, child_page_num);
+  uint32_t child_max_key = get_node_max_key(child);
+  uint32_t index = internal_node_find_child(parent, child_max_key);
+
+  uint32_t original_num_keys = *internal_node_num_keys(parent);
+  *internal_node_num_keys(parent) = original_num_keys + 1;
+
+  if (original_num_keys >= INTERNAL_NODE_MAX_CELLS) {
+    printf("Need to implement splitting internal node");
+    exit(EXIT_FAILURE);
+  }
+  uint32_t right_child_page_num = *internal_node_right_child(parent);
+  void *right_child = get_page(table->pager, right_child_page_num);
+
+  if (child_max_key > get_node_max_key(right_child)) {
+    /* Replace right child */
+    *internal_node_child(parent, original_num_keys) = right_child_page_num;
+    *internal_node_key(parent, original_num_keys) =
+        get_node_max_key(right_child);
+    *internal_node_right_child(parent) = child_page_num;
+  } else {
+    /* Make room for the new cell */
+    for (uint32_t i = original_num_keys; i > index; i--) {
+      void *destination = internal_node_cell(parent, i);
+      void *source = internal_node_cell(parent, i - 1);
+      memcpy(destination, source, INTERNAL_NODE_CELL_SIZE);
+    }
+    *internal_node_child(parent, index) = child_page_num;
+    *internal_node_key(parent, index) = child_max_key;
+  }
+}
+
 void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value) {
   /*
   Create a new node and move half the cells over.
@@ -384,9 +457,11 @@ void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value) {
   Update parent or create a new parent.
   */
   void *old_node = get_page(cursor->table->pager, cursor->page_num); // low half
+  uint32_t old_max = get_node_max_key(old_node);
   uint32_t new_page_num = get_unused_page_num(cursor->table->pager);
   void *new_node = get_page(cursor->table->pager, new_page_num); // high half
   initialize_leaf_node(new_node);
+  *node_parent(new_node) = *node_parent(old_node);
   *leaf_node_next_leaf(new_node) = *leaf_node_next_leaf(old_node); // linkInsert
   *leaf_node_next_leaf(old_node) = new_page_num;
   /*
@@ -420,8 +495,13 @@ void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value) {
   if (is_node_root(old_node)) {
     return create_new_root(cursor->table, new_page_num);
   } else {
-    printf("Need to implement updating parent after split");
-    exit(EXIT_FAILURE);
+    uint32_t parent_page_num = *node_parent(old_node);
+    uint32_t new_max = get_node_max_key(old_node);
+    void *parent = get_page(cursor->table->pager, parent_page_num);
+
+    update_internal_node_key(parent, old_max, new_max);
+    internal_node_insert(cursor->table, parent_page_num, new_page_num);
+    return;
   }
 }
 
@@ -477,22 +557,10 @@ Cursor *leaf_node_find(Table *table, uint32_t page_num, uint32_t key) {
 
 Cursor *internal_node_find(Table *table, uint32_t page_num, uint32_t key) {
   void *node = get_page(table->pager, page_num);
-  uint32_t num_keys = *internal_node_num_keys(node);
 
-  /* Binary search to find index of child to search */
-  uint32_t min_index = 0;
-  uint32_t max_index = num_keys; /* there is one more child than key */
+  uint32_t child_index = internal_node_find_child(node, key);
+  uint32_t child_num = *internal_node_child(node, child_index);
 
-  while (min_index != max_index) {
-    uint32_t index = (min_index + max_index) / 2;
-    uint32_t key_to_right = *internal_node_key(node, index);
-    if (key_to_right >= key) {
-      max_index = index;
-    } else {
-      min_index = index + 1;
-    }
-  }
-  uint32_t child_num = *internal_node_child(node, min_index);
   void *child = get_page(table->pager, child_num);
   switch (get_node_type(child)) {
   case NODE_LEAF:
--- mdb_test.go
+++ mdb_test.go
@@ -17,6 +17,45 @@ func setupAndTeardownDBFile() string {
 	return dbPath
 }
 
+func TestAllowPrintingOutStructureOfFourLeafNodeBTree(t *testing.T) {
+	setupAndTeardownDBFile()
+	script := []string{"insert 18 user18 person18@example.com",
+		"insert 7 user7 person7@example.com",
+		"insert 10 user10 person10@example.com",
+		"insert 29 user29 person29@example.com",
+		"insert 23 user23 person23@example.com",
+		"insert 4 user4 person4@example.com",
+		"insert 14 user14 person14@example.com",
+		"insert 30 user30 person30@example.com",
+		"insert 15 user15 person15@example.com",
+		"insert 26 user26 person26@example.com",
+		"insert 22 user22 person22@example.com",
+		"insert 19 user19 person19@example.com",
+		"insert 2 user2 person2@example.com",
+		"insert 1 user1 person1@example.com",
+		"insert 21 user21 person21@example.com",
+		"insert 11 user11 person11@example.com",
+		"insert 6 user6 person6@example.com",
+		"insert 20 user20 person20@example.com",
+		"insert 5 user5 person5@example.com",
+		"insert 8 user8 person8@example.com",
+		"insert 9 user9 person9@example.com",
+		"insert 3 user3 person3@example.com",
+		"insert 12 user12 person12@example.com",
+		"insert 27 user27 person27@example.com",
+		"insert 17 user17 person17@example.com",
+		"insert 16 user16 person16@example.com",
+		"insert 13 user13 person13@example.com",
+		"insert 24 user24 person24@example.com",
+		"insert 25 user25 person25@example.com",
+		"insert 28 user28 person28@example.com",
+		".btree",
+		".exit",
+	}
+	result := runScript(script)
+	printResult(result)
+}
+
 func TestBTreeStructurePrinting(t *testing.T) {
 	setupAndTeardownDBFile()
 
@@ -263,6 +302,7 @@ func TestInsertAndRetrieveRow(t *testing.T) {
 }
 
 func TestTableFullErrorMessage(t *testing.T) {
+	setupAndTeardownDBFile()
 	var script []string
 	for i := 1; i <= 1401; i++ {
 		script = append(script, "insert "+strconv.Itoa(i)+" user"+strconv.Itoa(i)+" person"+strconv.Itoa(i)+"@example.com")
@@ -273,7 +313,7 @@ func TestTableFullErrorMessage(t *testing.T) {
 
 	expectedErrorMessage := []string{
 		"db > Executed.",
-		"db > Need to implement updating parent after split",
+		"db > Need to implement splitting internal node",
 	}
 	result = result[len(result)-2:]
 	if !isEqual(result, expectedErrorMessage) {
@@ -337,4 +377,10 @@ func createAndRemoveDBFile() string {
 	dbPath := "test.db"
 	os.Remove(dbPath) // 删除已存在的数据库文件
 	return dbPath
+}
+
+func printResult(result []string) {
+	for _, line := range result {
+		fmt.Println(line)
+	}
 }
